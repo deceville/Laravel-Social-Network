65a4f4d8b0e9d8ff7447a9024582e0e882856a32
Decebel Geduriagao <Decebel Geduriagao>
2017-01-01 18:25:27 +0800

    Editing Posts with AJAX Request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -43,3 +43,11 @@ Route::get('/delete-post/{post_id}',
 		'middleware' => 'auth'
 
 ]);
+Route::post('/edit', function(\Illuminate\Http\Request $request) {
+	return response()->json(['message' => $request['postId']]);
+	/*
+	* {
+	*	message: '$request['body']'
+	* }
+	*/
+})->name('edit');
