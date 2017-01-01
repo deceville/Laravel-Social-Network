29371a230e2941a01d9b0c334e9125f93a040376
Decebel Geduriagao <Decebel Geduriagao>
2017-01-01 14:11:14 +0800

    Added Signup Controller and Model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-6">
             <h3>Sign Up</h3>
-            <form action="#" method="post">
+            <form action="{{ route('signup') }}" method="post">
                 <div class="form-group">
                     <label for="email">Your E-mail</label>
                     <input class="form-control" type="text" name="email" id="email"></input>
@@ -22,6 +22,7 @@
                     <input class="form-control" type="text" name="password" id="password"></input>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token[] }}"></input>
             </form>
         </div>
         <div class="col-md-6">
