3cd9fe0a98be5a57423aca6babc88bb34b72b168
Decebel Geduriagao <Decebel Geduriagao>
2017-01-01 19:14:18 +0800

    Adding User Account View

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,6 +27,14 @@ Route::get('/logout',
 	['uses' => 'UserController@getLogout',
 		'as' => 'logout'
 ]);
+Route::get('/account', 
+	['uses' => 'UserController@getAccount',
+    'as' => 'account'
+]);
+Route::post('/upateaccount',
+    ['uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
+]);
 Route::get('/dashboard', 
 	['uses' => 'PostController@getDashboard',
 		'as' => 'dashboard',
