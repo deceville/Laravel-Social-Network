df654f2a858eea395a00dec88f3c7da11ea3efe6
Decebel Geduriagao <Decebel Geduriagao>
2017-01-15 01:51:05 +0800

    Changes to welcome page

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <p>&copy; LOL Mania 2017</p>
+            </div>
+        </div>  
+    </div>
+</footer>
